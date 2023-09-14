72e1876ddf3d4c7f1c1c7af37fbe68e9edc8b656
Nurdin Djumabekov <dui.nurdin@gmail.com>
2023-09-14 21:04:11 +0600

    Add files via upload

--- матрица компентенции/public/js/app.js
+++ матрица компентенции/public/js/app.js
@@ -1,5 +1,44 @@
-const someFunction = () => {
-  const name = "nurdin";
-  return name;
+const root = document.getElementById("root");
+// import url from "./data.json";
+
+const url = "../data/data.json";
+
+const createElement = (parent, tagName, text, className) => {
+  const element = document.createElement(tagName);
+  element.setAttribute("class", className);
+  element.innerText = text;
+  parent.appendChild(element);
 };
-someFunction;()
+const arrTopics = [];
+const arrSubTopics = [];
+const level = [];
+
+const sendRequest = async () => {
+  try {
+    const response = await fetch(url);
+    const obj = await response.json();
+    console.log(obj);
+    for (const i in obj) {
+      arrTopics.push(i);
+      arrSubTopics.push(obj?.[i]);
+    }
+
+    obj?.devs.forEach((item) => {
+      console.log(item, "55");
+      level.push(item);
+    });
+
+    console.log(arrTopics, "arrTopics");
+    // console.log(arrSubTopics, "arrSubTopics");
+    const divParent = document.createElement("div");
+    arrTopics.forEach((item) => {
+      divParent.setAttribute("class", "divParent");
+      root.appendChild(divParent);
+      createElement(divParent, "h3", item, "title");
+    });
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+sendRequest();
