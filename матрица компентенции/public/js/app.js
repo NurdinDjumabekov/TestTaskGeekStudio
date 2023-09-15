fba9b82ed10eaf99c099ab59cb6aa68a783de69c
Nurdin Djumabekov <dui.nurdin@gmail.com>
2023-09-15 20:52:30 +0600

    Add files via upload

--- матрица компентенции/public/js/app.js
+++ матрица компентенции/public/js/app.js
@@ -1,19 +1,127 @@
+import { w2grid } from "https://rawgit.com/vitmalina/w2ui/master/dist/w2ui.es6.min.js";
 const root = document.getElementById("root");
-// import url from "./data.json";
 
 const url = "../data/data.json";
 
-const createElement = (parent, tagName, text, className) => {
-  const element = document.createElement(tagName);
-  element.setAttribute("class", className);
-  element.innerText = text;
-  parent.appendChild(element);
+function printKeys(obj) {
+  // for (const key in obj) {
+  //   console.log(key);
+  //   if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+  //     printKeys(obj[key]); // Рекурсивный вызов для вложенных объектов
+  //     console.log("////////////////////////");
+  //   }
+  // }
+}
+
+const renderTable = (obj, arrTopics, arrSubTopics) => {
+  let count = 0;
+  console.log(obj.devs, "tgyhuj");
+  const data = arrTopics.map(
+    (i, ind) => (
+      console.log(i),
+      count++,
+      {
+        recid: ind + 1,
+        start: i,
+        [obj.devs[count]]: outputTopic(obj[i]),
+      }
+    )
+  );
+
+  console.log(data, "data");
+  // console.log(arrTopics, "arrTopics");
+  new w2grid({
+    name: "root",
+    box: "#root",
+    columns: newColumnsArr,
+    records: data,
+    // records: [
+    //   {
+    //     recid: 1,
+    //     start: "Вёрстка",
+    //     Intern: `
+    //           <h2>Разметка HTML</h2>
+    //           <i>что такое HTML
+    //           теги, атрибуты, значения
+    //           базовые элементы</i>
+    //           <h2>Основы CSS</h2>
+    //           <i>принцип работы CSS</i>
+    //           <h2>CSS-селекторы</h2>
+    //           <i>базовые селекторы (элементы, классы, ID)</i>
+    //           `,
+    //     Junior: `<h2>Разметка HTML</h2>
+    //           <i>что такое HTML
+    //           теги, атрибуты, значения
+    //           базовые элементы</i>
+    //           <h2>Основы CSS</h2>
+    //           <i>принцип работы CSS</i>
+    //           <h2>CSS-селекторы</h2>
+    //           <i>базовые селекторы (элементы, классы, ID)</i>
+    //           `,
+    //     JuniorPlus: `<h2>Разметка HTML</h2>
+    //           <i>что такое HTML
+    //           теги, атрибуты, значения
+    //           базовые элементы</i>
+    //           `,
+    //   },
+    //   // {
+    //   //   recid: 2,
+    //   //   start: "Основы JavaScript",
+    //   //   Intern: `
+    //   //         <h2>Разметка HTML</h2>
+    //   //         <i>что такое HTML
+    //   //         теги, атрибуты, значения
+    //   //         базовые элементы</i>
+    //   //         <h2>Основы CSS</h2>
+    //   //         <i>принцип работы CSS</i>
+    //   //         <h2>CSS-селекторы</h2>
+    //   //         <i>базовые селекторы (элементы, классы, ID)</i>
+    //   //         `,
+    //   // },
+    //   // {
+    //   //   recid: 3,
+    //   //   start: "HTTP, DOM, архитектура",
+    //   // },
+    // ],
+  });
+  // console.log(arrTopics, arrSubTopics);
 };
-const arrTopics = [];
-const arrSubTopics = [];
-const level = [];
+
+const columnsArr = [
+  {
+    level: "start",
+    name: "start",
+  },
+  {
+    level: "Intern",
+    name: "Intern",
+  },
+  {
+    level: "Junior",
+    name: "Junior",
+  },
+
+  {
+    level: "Junior+",
+    name: "Junior1",
+  },
+  {
+    level: "Junior++",
+    name: "Junior2",
+  },
+  {
+    level: "Strong Junior",
+    name: "StrongJunior",
+  },
+  {
+    level: "Strong Junior",
+    name: "StrongJunior",
+  },
+];
 
 const sendRequest = async () => {
+  const arrTopics = [];
+  const arrSubTopics = [];
   try {
     const response = await fetch(url);
     const obj = await response.json();
@@ -22,23 +130,28 @@ const sendRequest = async () => {
       arrTopics.push(i);
       arrSubTopics.push(obj?.[i]);
     }
+    printKeys(obj);
 
-    obj?.devs.forEach((item) => {
-      console.log(item, "55");
-      level.push(item);
-    });
-
-    console.log(arrTopics, "arrTopics");
-    // console.log(arrSubTopics, "arrSubTopics");
-    const divParent = document.createElement("div");
-    arrTopics.forEach((item) => {
-      divParent.setAttribute("class", "divParent");
-      root.appendChild(divParent);
-      createElement(divParent, "h3", item, "title");
-    });
-  } catch (error) {
-    console.error("Error:", error);
+    renderTable(obj, arrTopics, arrSubTopics);
+  } catch (err) {
+    console.error("Error:", err);
   }
 };
 
+const newColumnsArr = columnsArr.map((i) => ({
+  field: i.level,
+  text: i.level,
+  size: i.level === "start" ? "150px" : "250px",
+}));
+
 sendRequest();
+
+const outputTopic = (topic) => {
+  // console.log(topic, "ftyguhk");
+  let text = "";
+  for (const key in topic) {
+    console.log(key);
+    text += `<h2>${key}</h2>`;
+  }
+  return text;
+};
